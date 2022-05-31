0e51e447ea107107ef8a25d7923114ef6c0a6913
Charles Miller <shadowmil@gmail.com>
2022-05-31 16:10:08 -0400

    Rename directories

--- module/helpers/templates.mjs
+++ module/helpers/templates.mjs
@@ -7,9 +7,9 @@
   return loadTemplates([
 
     // Actor partials.
-    "systems/boilerplate/templates/actor/parts/actor-features.html",
-    "systems/boilerplate/templates/actor/parts/actor-items.html",
-    "systems/boilerplate/templates/actor/parts/actor-spells.html",
-    "systems/boilerplate/templates/actor/parts/actor-effects.html",
+    "systems/fivetorchesdeep/templates/actor/parts/actor-features.html",
+    "systems/fivetorchesdeep/templates/actor/parts/actor-items.html",
+    "systems/fivetorchesdeep/templates/actor/parts/actor-spells.html",
+    "systems/fivetorchesdeep/templates/actor/parts/actor-effects.html",
   ]);
 };
--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -10,7 +10,7 @@ export class BoilerplateActorSheet extends ActorSheet {
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
       classes: ["boilerplate", "sheet", "actor"],
-      template: "systems/boilerplate/templates/actor/actor-sheet.html",
+      template: "systems/fivetorchesdeep/templates/actor/actor-sheet.html",
       width: 600,
       height: 600,
       tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "features" }]
@@ -19,7 +19,7 @@ export class BoilerplateActorSheet extends ActorSheet {
 
   /** @override */
   get template() {
-    return `systems/boilerplate/templates/actor/actor-${this.actor.data.type}-sheet.html`;
+    return `systems/fivetorchesdeep/templates/actor/actor-${this.actor.data.type}-sheet.html`;
   }
 
   /* -------------------------------------------- */
