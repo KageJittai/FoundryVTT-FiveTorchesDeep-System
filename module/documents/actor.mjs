45de3d6d6fd220e515e04bfc9ce7f4e15c4af3a6
Charles Miller <shadowmil@gmail.com>
2022-05-31 20:49:47 -0400

    rename actor flags

--- module/documents/actor.mjs
+++ module/documents/actor.mjs
@@ -31,7 +31,7 @@ export class ActorFTD extends Actor {
   prepareDerivedData() {
     const actorData = this.data;
     const data = actorData.data;
-    const flags = actorData.flags.boilerplate || {};
+    const flags = actorData.flags.ftd || {};
 
     // Make separate methods for each Actor type (character, npc, etc.) to keep
     // things organized.
